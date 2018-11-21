6cee8d0a4a195f0218685dced4147b1dfc855af9
João Bosco de Barros Freitas <jbbfreitas@gmail.com>
2018-11-21 20:01:59 -0300

    Segundo commit
    * Criei mais dois system.out

--- src/primo/Main.java
+++ src/primo/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
        System.out.println("Ola mundo");
+       System.out.println("Testando o git");
+       System.out.println("Legal");
 		
 		
 	}
